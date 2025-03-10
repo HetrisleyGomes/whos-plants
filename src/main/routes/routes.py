9a442da5bd5f6a0ff44af197465a40d7ea0f4e31
unknown <hetrisleygomes@gmail.com>
2025-03-10 11:43:31 -0300

    FEAT: Adicionado modo da versão chinesa

--- src/main/routes/routes.py
+++ src/main/routes/routes.py
@@ -1,30 +1,64 @@
-from flask import render_template, url_for, request, redirect, Blueprint, jsonify
+from flask import render_template, url_for, request, redirect, Blueprint
 import pandas as pd
-import json, os, random
+import random
 from datetime import datetime
 
 main_bp = Blueprint("main_bp", __name__)
 
 
 file_path = 'data/PVZ.xlsx'
+last_sheet = ''
+
 used_values = []
 choosed_names = []
+chinease_used_values = []
+chinease_choosed_names = []
+
 victory = False
+chinease_victory = False
+number_of_trys = [0,0,0]
 
 @main_bp.route("/")
 @main_bp.route("/index")
 def index():
+    if last_sheet != 'Database':
+        global rand, data
+        rand = gerar_numero_aleatorio('Database')
+        data = get_data('Database')
+
     names = data['Name'].tolist()
 
     return render_template(
         "index.html",
         data = data,
         names = names,
-        rows = get_rows(),
+        rows = get_rows(0),
         used_values = used_values,
         choosed_names = choosed_names,
         rand = rand,
-        victory = victory
+        victory = victory,
+        number_of_trys = number_of_trys
+        )
+
+@main_bp.route("/chinease")
+def chinease():
+    if last_sheet != 'Chinese_Database':
+        global rand, data
+        rand = gerar_numero_aleatorio('Chinese_Database')
+        data = get_data('Chinese_Database')
+
+    names = data['Name'].tolist()
+
+    return render_template(
+        "chinease_version.html",
+        data = data,
+        names = names,
+        rows = get_rows(1),
+        used_values = chinease_used_values,
+        choosed_names = chinease_choosed_names,
+        rand = rand,
+        victory = chinease_victory,
+        number_of_trys = number_of_trys
         )
 
 @main_bp.route("/makeguess", methods=["GET","POST"])
@@ -33,22 +67,33 @@ def guesser():
     print(guess.capitalize())
     item = data[data['Name'].str.lower() == guess.lower()]
     global victory
+    used_values.append(int(item['ID'].iloc[0]))
+    choosed_names.append(item['Name'].iloc[0])
     if data['Name'][rand-1] == item['Name'].iloc[0]:
-        print("Acertou")
-        used_values.append(int(item['ID'].iloc[0]))
-        choosed_names.append(item['Name'].iloc[0])
         victory = True
-    else:
-        print('errou')
-        used_values.append(int(item['ID'].iloc[0]))
-        choosed_names.append(item['Name'].iloc[0])
+        number_of_trys[0] = used_values.__len__()
+
     return redirect(url_for('main_bp.index'))
     
+@main_bp.route("/makeguess_chinease", methods=["GET","POST"])
+def guesser_chinease():
+    guess = request.form.get('guess')
+    print(guess.capitalize())
+    item = data[data['Name'].str.lower() == guess.lower()]
+    global chinease_victory
+    chinease_used_values.append(int(item['ID'].iloc[0]))
+    chinease_choosed_names.append(item['Name'].iloc[0])
+    if data['Name'][rand-1] == item['Name'].iloc[0]:
+        chinease_victory = True
+        number_of_trys[1] = chinease_used_values.__len__()
 
-def gerar_numero_aleatorio():
+    return redirect(url_for('main_bp.chinease'))
+
+def gerar_numero_aleatorio(sheet_name):
+    global last_sheet
+    last_sheet = sheet_name
     # Carregar o arquivo Excel
-    file_path = 'data/PVZ.xlsx'
-    data = pd.read_excel(file_path, sheet_name='Database')
+    data = pd.read_excel(file_path, sheet_name=sheet_name)
     df = data[data['Name'].notna()]  # remove apenas as linhas onde todos os valores são NaN
 
     # Obter o número de ocorrências (número de linhas no arquivo)
@@ -73,17 +118,18 @@ def gerar_numero_aleatorio():
     return numero_aleatorio
 
 
-def get_data():
-    file_path = 'data/PVZ.xlsx'
-    data = pd.read_excel(file_path, sheet_name='Database', header=0)
+def get_data(sheet_name):
+    global last_sheet
+    last_sheet = sheet_name
+    data = pd.read_excel(file_path, sheet_name=sheet_name, header=0)
     df = data[data['Name'].notna()]
 
     return df
 
-def get_rows():
+def get_rows(version):
     rows = []
-    for value in used_values:
-        print(data['Recarga'].iloc[value - 1])
+    values_to_use = used_values if version == 0 else chinease_used_values
+    for value in values_to_use:
         row = {
             'name': data['Name'].iloc[value - 1],
             'type': data['Type'].iloc[value - 1],
@@ -94,16 +140,18 @@ def get_rows():
             'rarity': data['Rarity'].iloc[value - 1],
             'usage': [data['Single use'].iloc[value - 1],data['Instant use'].iloc[value - 1]],
             'plant_origin': data['Plant_Origin'].iloc[value -1],
-            'origin': data['Origin'].iloc[value - 1]
+            'origin': data['Origin'].iloc[value - 1],
+            'classification': data['Classification'].iloc[value - 1],
         }
         rows.append(row)
-
     rows.reverse()
     
     return rows
 
 
+if last_sheet == '':
+    last_sheet = 'Database'
 # Testar a função
-rand = gerar_numero_aleatorio()
+rand = gerar_numero_aleatorio(last_sheet)
 #print(f"O número aleatório gerado para hoje é: {rand}")
-data = get_data()
+data = get_data(last_sheet)
